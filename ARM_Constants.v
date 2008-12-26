349ce587615d44d648ff162fb9d142466281d322
Joshua Wise <joshua@rebirth.joshuawise.com>
2008-12-26 01:20:45 -0500

    Add more constants.

--- ARM_Constants.v
+++ ARM_Constants.v
@@ -14,3 +14,22 @@
 `define COND_LE 4'b1101	/* Z set OR (N not equals V) */
 `define COND_AL 4'b1110	/* TRUE */
 `define COND_NV 4'b1111	/* FALSE */
+
+`define COND_MATTERS(x) ((x != `COND_AL) && (x != `COND_NV))
+
+`define ALU_AND 4'b0000
+`define ALU_EOR 4'b0001
+`define ALU_SUB 4'b0010
+`define ALU_RSB 4'b0011
+`define ALU_ADD 4'b0100
+`define ALU_ADC 4'b0101
+`define ALU_SBC 4'b0110
+`define ALU_RBC 4'b0111
+`define ALU_TST 4'b1000
+`define ALU_TEQ 4'b1001
+`define ALU_CMP 4'b1010
+`define ALU_CMN 4'b1011
+`define ALU_ORR 4'b1100
+`define ALU_MOV 4'b1101
+`define ALU_BIC 4'b1110
+`define ALU_MVN 4'b1111
