3f5cefe1c95feae38570a648b2d1a2bc021919b7
Joshua Wise <joshua@rebirth.joshuawise.com>
2008-12-28 03:24:25 -0500

    elanus

--- Execute.v
+++ Execute.v
@@ -9,15 +9,13 @@ module Execute(
 	input [31:0] pc,
 	input [31:0] insn,
 	input [31:0] cpsr,
-	input reg [31:0] op0,
-	input reg [31:0] op1,
-	input reg [31:0] op2,
-	input reg carry,
+	input [31:0] op0,
+	input [31:0] op1,
+	input [31:0] op2,
+	input carry,
 	
 	output reg outstall = 0,
-	output reg outbubble = 1,
+	output reg outbubble = 1
 	);
-
-	
 	
 endmodule
