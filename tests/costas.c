4aa2768b7a9c6eff4e9c26daba0b46ded3fe3a71
Joshua Wise <joshua@rebirth.joshuawise.com>
2009-01-10 04:11:45 -0500

    Add tests directory.

---
+++ tests/costas.c
@@ -0,0 +1,22 @@
+extern void putc(unsigned char c);
+
+int main()
+{
+//	unsigned char *costas = "Costas likes ass";
+	
+	putc('A');
+	putc('n');
+	putc('u');
+	putc('s');
+	putc('?');
+	
+//	while (*costas)
+//	{
+//		putc(*costas);
+//		costas++;
+//	}
+	while(1)
+		;
+	
+	return 0;
+}
