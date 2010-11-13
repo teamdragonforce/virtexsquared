bb9536bd206999d271f908a502a96f425711215d
Joshua Wise <jwise@andrew.cmu.edu>
2010-11-13 04:56:11 -0800

    core/ICache: Module ICache is correct for synthesis.  Tool XST is not.

--- rtl/core/ICache.v
+++ rtl/core/ICache.v
@@ -76,16 +76,23 @@ module ICache(/*AUTOARG*/
 	
 	reg [NWAYS-1:0] cache_valid [15:0];
 	reg [NWAYS_HI:0] cache_evict_next [15:0];
-	reg [(NWAYS*22)-1:0] cache_tags [15:0];
-	reg [(NWAYS*64)-1:0] cache_data [127:0 /* {line,word} */];	//synthesis attribute ram_style of cache_data is block
+	reg [21:0] cache_tags [(16 * NWAYS)-1:0];
 	
+	/* XXX: Xilinx is on drugs, and sometimes initializes cache_valid to
+	 * 1 until the system is reset.  So, as a workaround, we initialize
+	 * cache_tags to 22{1'b1}, which is a value that the world shall
+	 * never see.
+	 */
 	integer i;
+	integer j;
+	genvar gi;
 	initial
 		for (i = 0; i < 16; i = i + 1)
 		begin
-			cache_valid[i[3:0]] = {NWAYS{1'b0}};
-			cache_tags[i[3:0]] = {NWAYS{22'b0}};
-			cache_evict_next[i[3:0]] = {(NWAYS_HI+1){1'b0}};
+			cache_valid[i] = {NWAYS{1'b0}};
+			for (j = 0; j < NWAYS; j = j + 1)
+				cache_tags[i * NWAYS + j] = {22{1'b1}};
+			cache_evict_next[i] = {(NWAYS_HI+1){1'b0}};
 		end
 	
 	wire  [5:0] rd_didx_0a      = ic__rd_addr_0a[5:0];
@@ -95,16 +102,26 @@ module ICache(/*AUTOARG*/
 	
 	reg  [31:0] rd_addr_1a = 32'hFFFFFFFF;
 
-	reg cache_hit_0a;
-	reg [NWAYS_HI:0] cache_hit_way_0a;
+	reg cache_hit_0a = 0;
+	wire [NWAYS-1:0] cache_hit_ways_0a;
+	
+	reg [NWAYS_HI:0] cache_hit_way_0a = 0;
 
 	reg cache_hit_1a = 1'b0;
 	reg [NWAYS_HI:0] cache_hit_way_1a = {(NWAYS_HI+1){1'bx}};
+	wire [NWAYS-1:0] cache_valid_cur_idx = cache_valid[rd_idx_0a];	/* :fu -100, xst. */
+	
+	generate
+	for (gi = 0; gi < NWAYS; gi = gi + 1) begin: hit_ways_0a
+		assign cache_hit_ways_0a[gi] = cache_valid_cur_idx[gi] && (cache_tags[rd_idx_0a*NWAYS + gi] == rd_tag_0a);
+	end
+	endgenerate
+	
 	always @(*) begin
 		cache_hit_0a = 0;
 		cache_hit_way_0a = {(NWAYS_HI+1){1'bx}};
 		for (i = 0; i < NWAYS; i = i + 1) begin
-			if (cache_valid[rd_idx_0a][i] && (cache_tags[rd_idx_0a][(22*(i+1))-1-:22] == rd_tag_0a)) begin
+			if (cache_hit_ways_0a[i]) begin
 				cache_hit_0a = 1;
 				cache_hit_way_0a = i[NWAYS_HI:0];
 			end
@@ -123,25 +140,18 @@ module ICache(/*AUTOARG*/
 
 	/*** Processor control bus logic ***/
 	reg [31:0] ic__rd_addr_1a = 0;
-	reg [(NWAYS*64)-1:0] curdata_1a = 0;
-	reg [63:0] curdata_curway_1a = 0;
+	wire [63:0] curdata_1a [NWAYS-1:0];	/* Assigned later by data bank generation. */
+	wire [63:0] curdata_curway_1a = curdata_1a[cache_hit_way_1a];
 	
 	always @(*) begin
-		/* verilator lint_off WIDTH */
-		curdata_curway_1a = curdata_1a[(cache_hit_way_1a+1)*64-1-:64];
-		/* verilator lint_on WIDTH */
-		/* and can suck my dick, that is totally fine */
-		
 		ic__rd_wait_0a = ic__rd_req_0a && !cache_hit_0a;
 		ic__rd_data_1a = ic__rd_addr_1a[2] ? curdata_curway_1a[63:32] : curdata_curway_1a[31:0];
 	end
 	always @(posedge clk or negedge rst_b) begin
 		if (!rst_b) begin
-			curdata_1a <= {(64*NWAYS){1'b0}};
 			ic__rd_addr_1a <= 0;
 		end else begin
 			// Do the actual read.
-			curdata_1a <= cache_data[{rd_idx_0a,rd_didx_word_0a}];
 			ic__rd_addr_1a <= ic__rd_addr_0a;
 		end
 	end
@@ -197,10 +207,29 @@ module ICache(/*AUTOARG*/
 	reg completed_read_fclk = 0;
 	reg completed_read_s1 = 0;
 	reg completed_read = 0;
+	
+	/* XST can eat it.  Apparently I have to decompose all of my logic
+	 * into primitive instantatiations if I want xst to not
+	 * mis-synthesize.
+	 */
+	reg [NWAYS-1:0] cache_valid_next = 0;
+	wire [NWAYS-1:0] cache_valid_cur = cache_valid[fill_idx];
+	always @(*)
+		for (i = 0; i < NWAYS; i = i + 1)
+			cache_valid_next[i] =
+				(i[NWAYS_HI:0] == fill_evict) ?
+					(start_read) ? 1'b0 :
+					((completed_read == current_read) && read_pending) ? 1'b1 :
+					cache_valid_cur[i] :
+				cache_valid_cur[i];
+	
 	always @(posedge clk or negedge rst_b) begin
 		if (!rst_b) begin
 			for (i = 0; i < 16; i = i + 1)
 				cache_valid[i] <= {NWAYS{1'b0}};
+			for (i = 0; i < 16; i = i + 1)
+				for (j = 0; j < NWAYS; j = j + 1)
+					cache_tags[i * NWAYS + j] <= {22{1'b1}};
 			read_pending <= 0;
 			fill_addr <= 0;
 			completed_read <= 0;
@@ -209,16 +238,21 @@ module ICache(/*AUTOARG*/
 		end else begin
 			completed_read_s1 <= completed_read_fclk;
 			completed_read <= completed_read_s1;
+			cache_valid[fill_idx] <= cache_valid_next;
 		
 			if (start_read) begin
 				read_pending <= 1;
 				current_read <= ~current_read;
 				fill_addr <= {ic__rd_addr_0a[31:6], 6'b0};
-				cache_valid[fill_idx][fill_evict] <= 1'b0;
+				/* The actual logic here:
+				 * cache_valid[fill_idx][fill_evict] <= 1'b0;
+				 */
 			end else if ((completed_read == current_read) && read_pending) begin
 				/* verilator lint_off WIDTH */
-				cache_tags[fill_idx][(fill_evict+1)*22-1-:22] <= fill_tag;
-				cache_valid[fill_idx][fill_evict] <= 1'b1;
+				cache_tags[fill_idx * NWAYS + fill_evict] <= fill_tag;
+				/* The actual logic here:
+				 * cache_valid[fill_idx][fill_evict] <= 1'b1;
+				 */
 				cache_evict_next[fill_idx] <= (fill_evict == (NWAYS - 1)) ? 0 : fill_evict + 1;
 				/* verilator lint_on WIDTH */
 				read_pending <= 0;
@@ -264,23 +298,49 @@ module ICache(/*AUTOARG*/
 				
 				if (cache_fill_pos_fclk == 7)	/* Done? */
 					completed_read_fclk <= current_read_fclk;
-				/* verilator lint_off WIDTH */
-				cache_data[{fill_idx,cache_fill_pos_fclk}][(fill_evict+1)*64-1-:64] <= fsabi_data[63:0];
-				/* verilator lint_on WIDTH */
 				cache_fill_pos_fclk <= cache_fill_pos_fclk + 1;
+				
+				/* Actual fill logic moved to databank control logic. */
 			end
 		end
 	end
+	
+	/*** Cache data bank control logic. ***/
+	generate
+	for (gi = 0; gi < NWAYS; gi = gi + 1) begin: cache_way
+		reg [63:0] cache_data [127:0 /* {line,word} */];	//synthesis attribute ram_style of cache_data is block
+		reg [63:0] local_curdata_1a = 0;
+		
+		/* All reads happen in parallel; writes are selective. */
+		assign curdata_1a[gi] = local_curdata_1a[63:0];
+		
+		/* Reset NOT allowed here, because block RAM. */
+		always @(posedge clk) begin
+			local_curdata_1a <= cache_data[{rd_idx_0a,rd_didx_word_0a}];
+		end
+		
+		always @(posedge fsabi_clk) begin
+			if (fsabi_valid && (fsabi_did == FSAB_DID_CPU) && (fsabi_subdid == FSAB_SUBDID_CPU_ICACHE) && (fill_evict == gi[NWAYS_HI:0])) begin
+				cache_data[{fill_idx,cache_fill_pos_fclk}] <= fsabi_data[63:0];
+			end
+		end
+	end
+	endgenerate
 	
 	/*** Chipscope visibility ***/
 	generate
 	if (DEBUG == "TRUE") begin: debug
 		chipscope_ila ila2 (
 			.CONTROL(ic__control2), // INOUT BUS [35:0]
-			.CLK(fsabi_clk), // IN
-			.TRIG0({fsabi_rst_b, current_read_fclk, completed_read_fclk, cache_fill_pos_fclk[2:0],
-			        fsabi_valid, fsabi_did[3:0], fsabi_subdid[3:0],
-			        ic__fsabo_credit, ic__fsabo_valid, fsab_credits[2:0], fill_addr[31:0]})
+			.CLK(clk), // IN
+			
+			.TRIG0({cache_valid[rd_idx_0a], cache_tags[{rd_idx_0a,1'b0}], cache_tags[{rd_idx_0a,1'b1}], ic__rd_addr_0a, ic__rd_data_1a, cache_hit_0a, cache_hit_way_0a,
+			        cache_valid_cur_idx, cache_hit_ways_0a,
+			        ic__rd_req_0a, read_pending,
+			        fsab_credit_avail, start_read, fill_evict,
+			        fill_idx, cache_valid_next,
+			        cache_valid_cur})
+			
 		);
 	
 	end else begin: debug_tieoff
