9492fe812b454946a41a0da4849cfa1d0811fe0c
Josiah Boning <jboning@gmail.com>
2010-11-15 11:41:41 -0500

    Keyboard handling code, ripped from 15-410.

---
+++ sw/keyboard/boot1.c
@@ -0,0 +1,121 @@
+#include "serial.h"
+#include "sysace.h"
+#include "keyhelp.h"
+
+#define SAMPLE_RATE 48000
+#define F 1000
+#define T (SAMPLE_RATE / F)
+
+struct ptab {
+	unsigned char flags;
+	unsigned char sh, scl, sch;
+	unsigned char type;
+	unsigned char eh, ecl, ech;
+	unsigned char lba[4];
+	unsigned char size[4];
+};
+
+int find_fat16()
+{
+	static unsigned char buf[512];
+	struct ptab *table;
+	int i;
+	
+	if (sysace_readsec(0, (unsigned int *)buf) < 0)
+	{
+		puts("failed to read sector 0!\r\n");
+		return -1;
+	}
+	
+	table = (struct ptab *)&(buf[446]);
+	for (i = 0; i < 4; i++)
+	{
+		if (table[i].type == 0x06 /* FAT16 */)
+		{
+			puts("(partition ");
+			puthex(i);
+			puts(") ");
+			return table[i].lba[0] |
+			       table[i].lba[1] << 8 |
+			       table[i].lba[2] << 16 |
+			       table[i].lba[3] << 24;
+		}
+	}
+	
+	return -1;
+}
+
+//#define LEN 85258656
+#define LEN 85258624
+//#define LEN    1000032
+
+void startplayback()
+{
+	volatile int *dma_start  = (int*) 0x84000000;
+	volatile int *dma_length = (int*) 0x84000004;
+	volatile int *dma_cmd    = (int*) 0x84000008;
+	volatile int *dma_nread  = (int*) 0x8400000c;
+
+	*dma_start = 0x00800000;
+	*dma_length = LEN & ~0xFF;
+	puthex(LEN & ~0xFF);
+	*dma_cmd = 2;
+}
+
+void loadaudio()
+{
+	int location = find_fat16();
+	unsigned int *base = (int*) 0x00800000;
+	int i;
+
+	if (location < 0)
+	{
+		puts("no FAT16-now-audio partition?\r\n");
+		return;
+	}
+	puts("Loading audio into memory (");
+	puthex(LEN/512);
+	puts(" sectors)... ");
+	for (i = 0; i < (LEN / 512); i++)
+	{
+		sysace_readsec(location + i, base);
+		base += (512/4);
+		if ((i & 0x1F) == 0)
+		{
+			puthex(i);
+			if (i == 0x20)	/* OK, we've loaded enough. */
+				startplayback();
+		}
+		puts(".");
+	}
+	puts("\r\n");
+}
+
+void main()
+{
+	loadaudio();
+
+	volatile short *master_vol = (int*) 0x84000100;
+	*master_vol = 0x0808;
+	puts("quieter!\r\n");
+	short *mem = (short*) (6 * (1<<20));
+	volatile unsigned int *scancodeaddr = 0x85000000;
+	unsigned int scancode;
+
+	kh_type k;
+
+	loadaudio();
+
+	while(1) {
+		scancode = *scancodeaddr;
+		if (scancode == 0xdeadbeef)
+			continue;
+		puthex(scancode);
+		k = process_scancode(scancode);
+		putchar(' ');
+		putchar(KH_GETCHAR(k));
+		puts("\r\n");
+	}
+
+	return 0;
+}
