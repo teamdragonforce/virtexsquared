f441b9e56934a6ac8805510efbc66b8ec93333ee
Joshua Wise <jwise@andrew.cmu.edu>
2010-11-28 20:31:07 -0500

    nogit: Remove *all* of the VCSen, since my laptop hasn't got Bzr either.

--- utils/nogit.el
+++ utils/nogit.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(vc-handled-backends (quote (RCS CVS SVN SCCS Bzr Hg Mtn Arch))))
+ '(vc-handled-backends (quote ())))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
