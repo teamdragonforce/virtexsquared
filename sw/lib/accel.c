04265e5943a38ee9c80dc47709da4af49a96ebd3
Joshua Wise <jwise@andrew.cmu.edu>
2010-12-03 08:37:09 -0500

    sw, accel: Add basic software for blitter, and make blitter correct for synthesis.

--- sw/lib/accel.c
+++ sw/lib/accel.c
@@ -1,20 +1,49 @@
 #include "minilib.h"
 #include "accel.h"
 
+#define P(x) (*(volatile unsigned int *)(x))
+
 void accel_fill(unsigned int *base, unsigned int value, unsigned int words)
 {
 	if (words % 8)
 		printf("ACCEL: fill word count not multiple of 2\r\n");
 	
-	*(volatile unsigned int *)FILL_VALUE = value;
-	*(volatile unsigned int *)FILL_ADDR = (unsigned int)base;
-	*(volatile unsigned int *)FILL_LENREM = words / 2;
+	P(FILL_VALUE) = value;
+	P(FILL_ADDR) = (unsigned int)base;
+	P(FILL_LENREM) = words / 2;
 	
-	while (*(volatile unsigned int *)FILL_LENREM != 0)
+	while (P(FILL_LENREM) != 0)
 	{
 		int i;
 		
 		for (i = 0; i < 100; i++)
-			*(volatile unsigned int *)FILL_VALUE; /* times out */
+			P(FILL_VALUE); /* times out */
+	}
+}
+
+void accel_blit(unsigned int *dest, unsigned int *src, unsigned int w, unsigned int h)
+{
+	int packets = (w / 16) * h;
+	
+	if (w % 16)
+		printf("ACCEL: blit width not multiple of 16 pxls\r\n");
+	if ((unsigned int)dest & 63)
+		printf("ACCEL: destination alignment for blit incorrect\r\n");
+	if ((unsigned int)src & 63)
+		printf("ACCEL: source alignment for blit incorrect\r\n");
+	
+	P(BLIT_WRADDR) = (unsigned int)dest;
+	P(BLIT_WRROWL) = w / 16;
+	P(BLIT_WRROWS) = 640 * 4;
+	P(BLIT_WRDONE) = 0;
+	
+	P(BLIT_RDADDR) = (unsigned int)src;
+	P(BLIT_RDLEN) = packets;
+	
+	while (P(BLIT_WRDONE) != packets)
+	{
+		int i;
+		for (i = 0; i < 10; i++)
+			P(BLIT_RDADDR);	/* times out */
 	}
 }
